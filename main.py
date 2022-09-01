203923dee6414489f639e5046ff427ba5958f751
Atikur Rabbi <msawon94@gmail.com>
2022-09-01 10:19:50 +0600

    update

--- main.py
+++ main.py
@@ -1,13 +1,5 @@
 from flask import Flask, render_template
 
-import os
-
-port = int(os.environ.get('PORT', 5000))
-
-app.run(host='0.0.0.0', port=port, debug=True)
-
-app = Flask(__name__)
-
 
 @app.route('/')
 def hello():
@@ -22,3 +14,5 @@ def test():
 def result():
    dict = {'phy':50,'che':60,'maths':70}
    return render_template('result.html', result = dict)
+   
+app.run(debug=True, port=33507)
